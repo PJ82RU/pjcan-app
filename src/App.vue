0dc8513e8ecaec94ab4c828d08ea09385165cfc3
PJ82 <project82@mail.ru>
2022-06-19 20:46:36 +0500

    Настройка App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
-	<div class="PJApp">
-		<router-view />
-	</div>
-
-	<MainMenu />
-	<!--<DialogBluetoothConnection />-->
-	<DialogUpdateFirmware />
+	<router-view />
 </template>
 
 <script lang="ts">
-import MainMenu from 'components/menu/MainMenu.vue';
-import DialogBluetoothConnection from 'components/bluetooth/dialogs/DialogBluetoothConnection.vue';
-import DialogUpdateFirmware from 'components/update-firmware/dialogs/DialogUpdateFirmware.vue';
 import { i18n } from './boot/i18n';
-
 const { name } = require('../package.json');
 
 export default {
 	name: 'PJCAN',
-	components: {
-		MainMenu,
-		DialogBluetoothConnection,
-		DialogUpdateFirmware
-	},
 	watch: {
 		'$route.name'(lang: string): void {
 			const _lang = i18n.t(lang);
@@ -31,12 +16,3 @@ export default {
 	}
 };
 </script>
-
-<style lang="sass">
-.PJApp
-	position: absolute
-	width: 100%
-	height: 100%
-	overflow: hidden
-	padding: 8px
-</style>
--- src/pages/Onboard.vue
+++ src/pages/Onboard.vue
@@ -1,73 +1,14 @@
 <template>
-	<MenuOnBoardButtons />
-	<Mazda />
-	<MazdaSpeedometer :speed="speed" :rpm="rpm" />
-	<MazdaOnboardData />
+	<div></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import MenuOnBoardButtons from '@/components/menu/MenuOnBoardButtons.vue';
-import MazdaSpeedometer from '@/components/mazda/MazdaSpeedometer.vue';
-import Mazda from '@/components/mazda/Mazda.vue';
-import MazdaOnboardData from '@/components/mazda/MazdaOnboardData.vue';
-
 export default defineComponent({
 	name: 'Onboard',
-	components: {
-		MenuOnBoardButtons,
-		MazdaSpeedometer,
-		Mazda,
-		MazdaOnboardData
-	},
-	data() {
-		return {
-			speed: 0,
-			rpm: 0
-		};
-	},
-	mounted() {
-		setTimeout(() => {
-			this.speed = 60;
-			this.rpm = 1500;
-		}, 5000);
-	}
+	components: {}
 });
 </script>
 
-<style lang="sass">
-/* портретная ориентация */
-@media screen and (orientation: portrait)
-    .MazdaSpeedometer
-        top: 150px
-        width: calc(100% - 16px)
-        display: flex
-        justify-content: center
-
-    #app .Mazda
-        height: 55%
-
-        &-position
-            display: none
-
-    .MazdaOnboardData
-        flex-direction: column
-        justify-content: flex-end
-        height: 45%
-
-/* горизонтальная ориентация */
-@media screen and (orientation: landscape)
-    .MazdaSpeedometer
-        right: 490px
-        top: 90px
-
-    .Mazda
-        width: 65%
-        height: calc(65% - 32px)
-        margin-bottom: 32px
-
-    .MazdaOnboardData
-        flex-direction: row
-        height: 35%
-</style>
+<style lang="sass"></style>
