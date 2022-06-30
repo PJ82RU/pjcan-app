74723613a0b784741f636bf224d088d1ca56c60e
PJ82 <project82@mail.ru>
2022-06-30 11:28:20 +0500

    Добавлен SelectCardSection

---
+++ src/components/common/CardSections/SelectCardSection.vue
@@ -0,0 +1,67 @@
+<template>
+	<q-card-section horizontal class="SelectCardSection">
+		<div class="SelectCardSection-title">{{ title }}</div>
+		<q-select
+			class="SelectCardSection-input"
+			outlined
+			dense
+			v-model="valueSelect"
+			:options="options"
+			:disable="disable"
+			:readonly="readonly"
+		/>
+	</q-card-section>
+</template>
+
+<script lang="ts">
+import { computed, toRefs } from 'vue';
+
+export default {
+	name: 'SelectCardSection',
+	props: {
+		title: {
+			type: String,
+			require: true
+		},
+		modelValue: {
+			type: String,
+			default: ''
+		},
+		options: {
+			type: Array,
+			default: []
+		},
+		disable: {
+			type: Boolean,
+			default: false
+		},
+		readonly: {
+			type: Boolean,
+			default: false
+		}
+	},
+	setup(props: any, context: any) {
+		const { modelValue } = toRefs(props);
+
+		const valueSelect = computed({
+			get(): string {
+				return modelValue.value;
+			},
+			set(value: string): void {
+				context.emit('update:modelValue', value);
+			}
+		});
+
+		return {
+			valueSelect
+		};
+	}
+};
+</script>
+
+<style lang="sass">
+@import "@/css/mixins"
+
+.SelectCardSection
+	@include card-section()
+</style>
