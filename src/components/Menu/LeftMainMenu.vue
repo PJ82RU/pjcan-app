ec9cb25621c72aec89bfe2c1e8b36357ae6f672c
PJ82 <project82@mail.ru>
2022-06-30 14:16:09 +0500

    Мелкие исправления

---
+++ src/components/Menu/LeftMainMenu.vue
@@ -0,0 +1,31 @@
+<template>
+	<q-list padding>
+		<MenuItem name="header" @click="onClickItem" />
+		<q-separator />
+		<MenuItem name="main" @click="onClickItem" />
+		<q-separator />
+		<MenuItem name="actions" @click="onClickItem" />
+		<q-separator />
+		<MenuItem name="footer" @click="onClickItem" />
+	</q-list>
+</template>
+
+<script lang="ts">
+import MenuItem from './MenuItem.vue';
+
+export default {
+	name: 'LeftMainMenu',
+	components: { MenuItem },
+	setup() {
+		const onClickItem = (e: any) => {
+			console.log(e);
+		};
+
+		return {
+			onClickItem
+		};
+	}
+};
+</script>
+
+<style lang="sass"></style>
