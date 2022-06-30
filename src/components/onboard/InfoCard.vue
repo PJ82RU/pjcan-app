07b0fdedf46727ea41a0bafd4fd3f6be8023682e
PJ82 <project82@mail.ru>
2022-06-30 12:40:40 +0500

    Реструктуризация

--- src/components/onboard/InfoCard.vue
+++ src/components/onboard/InfoCard.vue
@@ -1,104 +1,42 @@
 <!--suppress RequiredAttributes -->
 <template>
-	<q-card class="InfoCard">
-		<q-card-section horizontal>
-			<q-card-section :horizontal="false" class="InfoCard-container">
-				<q-card-section class="InfoCard-title">
-					<div class="text-h6">Общие показания</div>
-					<q-separator />
-				</q-card-section>
-
-				<q-card-section class="InfoCard-info">
-					<q-card-section horizontal>
-						<div class="InfoCard-info-variable">Время работы</div>
-						<q-input
-							class="InfoCard-info-time"
-							outlined
-							dense
-							readonly
-							v-model="time"
-							type="time"
-							step="2"
-						/>
-						<q-icon class="InfoCard-info-time-icon" color="primary" name="schedule" size="22px" />
-					</q-card-section>
-					<q-card-section horizontal>
-						<div class="InfoCard-info-variable">ACC</div>
-						<q-toggle class="InfoCard-info-value" dense disable v-model="value" color="red" />
-					</q-card-section>
-					<q-card-section horizontal>
-						<div class="InfoCard-info-variable">Температура за бортом</div>
-						<q-input class="InfoCard-info-value" outlined dense readonly v-model="text" />
-					</q-card-section>
-				</q-card-section>
-			</q-card-section>
-
-			<q-card-actions vertical class="InfoCard-menu">
-				<q-btn flat round color="secondary" icon="more_vert" />
-				<q-btn flat round color="primary" icon="bookmark" />
-			</q-card-actions>
-		</q-card-section>
-	</q-card>
+	<CardSection title="Тестовые данные">
+		<TimeCardSection title="Время работы" comment="Время туды-сюды" v-model="time" realtime />
+		<ToggleCardSection title="ACC" comment="То что током ебашит" v-model="value" />
+		<InputCardSection title="Температура за бортом" comment="Дубак на улице" v-model="text" readonly />
+		<InputCardSection title="Ввод числа" comment="Число число" v-model="text" type="number" :min="0" />
+		<SelectCardSection
+			title="Список"
+			comment="Писка насяльника"
+			v-model="select"
+			:options="selectOptions"
+			width="200px"
+		/>
+	</CardSection>
 </template>
 
 <script lang="ts">
+import CardSection from '@/components/common/CardSections/CardSection.vue';
+import TimeCardSection from '@/components/common/CardSections/TimeCardSection.vue';
+import ToggleCardSection from '@/components/common/CardSections/ToggleCardSection.vue';
+import InputCardSection from '@/components/common/CardSections/InputCardSection.vue';
+import SelectCardSection from '@/components/common/CardSections/SelectCardSection.vue';
+
 export default {
 	name: 'InfoCard',
+	components: { CardSection, TimeCardSection, ToggleCardSection, InputCardSection, SelectCardSection },
 	data() {
 		return {
 			value: true,
-			text: '25°C',
-			time: 0
+			//text: '25°C',
+			text: '',
+			time: '10:00:22',
+			num: Number(0),
+			select: 'тест',
+			selectOptions: ['тест', 'тест2']
 		};
 	}
 };
 </script>
 
-<style lang="sass">
-.InfoCard
-	width: 100%
-	max-width: 400px
-
-	.q-card__actions
-		justify-content: flex-start
-
-	&-container
-		background-color: #f5f5f5
-
-	&-title
-		padding-top: 10px
-		padding-bottom: 0
-
-		.text-h6
-			padding-bottom: 6px
-
-	&-container
-		width: 100%
-		padding: 0
-
-	&-info
-		padding-bottom: 0
-
-		.q-card__section
-			align-items: center
-			margin-bottom: 16px
-
-		&-variable
-			width: 100%
-
-		&-value
-			.q-field__inner
-				width: 80px
-			input
-				text-align: right
-
-		&-time
-			.q-field__inner
-				width: 120px
-			input
-				text-align: right
-
-			&-icon
-				position: absolute
-				right: 12px
-</style>
+<style lang="sass"></style>
