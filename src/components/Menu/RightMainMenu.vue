a732e00f69d0bfe804cf077201bcb85ce39c8475
PJ82 <project82@mail.ru>
2022-06-30 14:17:59 +0500

    Мелкие исправления

---
+++ src/components/Menu/RightMainMenu.vue
@@ -0,0 +1,30 @@
+<template>
+	<q-menu auto-close class="RightMainMenu" anchor="bottom left" transition-show="jump-down" transition-hide="jump-up">
+		<MenuItem name="popupDevice" @click="onClickItem" />
+		<q-separator />
+		<MenuItem name="popupMain" @click="onClickItem" />
+	</q-menu>
+</template>
+
+<script lang="ts">
+import MenuItem from '@/components/menu/MenuItem.vue';
+
+export default {
+	name: 'RightMainMenu',
+	components: { MenuItem },
+	setup() {
+		const onClickItem = (e: any) => {
+			console.log(e);
+		};
+
+		return {
+			onClickItem
+		};
+	}
+};
+</script>
+
+<style lang="sass">
+.RightMainMenu
+	min-width: 200px !important
+</style>
