de5e6cd24c8f9a1b461de04ba59e2d42fff0b637
PJ82 <project82@mail.ru>
2022-07-07 10:14:31 +0500

    VolumeCard: добавил настройки отображения

--- src/store/menu/MenuVolumeCard.ts
+++ src/store/menu/MenuVolumeCard.ts
@@ -1,6 +1,3 @@
 import { TItemMenu } from '@/models/menu/TItemMenu';
 
-export const menuVolumeCard = [
-	{ type: TItemMenu.VIEW_VOLUME_MUTE, lang: 'VolumeCard_Mute_Title' },
-	{ type: TItemMenu.VIEW_VOLUME_VOLUME, lang: 'VolumeCard_Volume_Title' }
-];
+export const menuVolumeCard = [{ type: TItemMenu.VIEW_VOLUME, lang: 'VolumeCard_Title' }];
