0d50121259e6a9dc1625f5845fe5edb7d7b174a0
PJ82 <project82@mail.ru>
2022-07-07 09:55:46 +0500

    EngineCard: добавил настройки отображения

--- src/components/onboard/EngineCard.vue
+++ src/components/onboard/EngineCard.vue
@@ -50,6 +50,12 @@
 			readonly
 		/>
 	</CardSection>
+	<ViewSettingModal
+		v-model="viewSettingModel"
+		:title="viewSettingTitle"
+		:view-config="viewConfig"
+		@apply="onApplyViewConfig"
+	/>
 </template>
 
 <script lang="ts">
@@ -61,9 +67,11 @@ import CardSectionToggle from '@/components/cardSections/CardSectionToggle.vue';
 import CardSectionInput from '@/components/cardSections/CardSectionInput.vue';
 import CardSectionProgress from '@/components/cardSections/CardSectionProgress.vue';
 import CardSection2Icons from '@/components/cardSections/CardSection2Icons.vue';
+import ViewSettingModal from '@/components/view/ViewSettingModal.vue';
 import { menuEngineCard } from '@/store/menu/MenuEngineCard';
-import { EngineValue, IEngineValue } from '@/models/pjcan';
-import api, { API_EVENT_VARIABLE_ENGINE } from '@/store/api';
+import { EngineValue, EngineView, IEngineValue, IViewConfig, TViewType } from '@/models/pjcan';
+import api, { API_EVENT_VARIABLE_ENGINE, API_EVENT_VARIABLE_ENGINE_VIEW } from '@/store/api';
+import { TItemMenu } from '@/models/menu';
 
 export default {
 	name: 'EngineCard',
@@ -73,19 +81,31 @@ export default {
 		CardSectionToggle,
 		CardSectionInput,
 		CardSectionProgress,
-		CardSection2Icons
+		CardSection2Icons,
+		ViewSettingModal
 	},
 	setup() {
+		// параметры ДВС
 		const engineValue = ref(new EngineValue());
-		const onReceive = (res: IEngineValue): void => {
+		const engineView = new EngineView();
+		// входящие значения ДВС
+		const onReceiveValue = (res: IEngineValue): void => {
 			engineValue.value.setModel(res);
 		};
+		// входящие значения отображения ДВС
+		const onReceiveView = (res: IEngineValue): void => {
+			engineView.setModel(res);
+		};
 
+		// регистрируем события
 		onMounted(() => {
-			api.addListener(API_EVENT_VARIABLE_ENGINE, onReceive);
+			api.addListener(API_EVENT_VARIABLE_ENGINE, onReceiveValue);
+			api.addListener(API_EVENT_VARIABLE_ENGINE_VIEW, onReceiveView);
 		});
+		// удаляем события
 		onUnmounted(() => {
-			api.removeListener(API_EVENT_VARIABLE_ENGINE, onReceive);
+			api.removeListener(API_EVENT_VARIABLE_ENGINE, onReceiveValue);
+			api.removeListener(API_EVENT_VARIABLE_ENGINE_VIEW, onReceiveView);
 		});
 
 		const enabled = computed((): boolean => engineValue.value.enabled);
@@ -96,8 +116,86 @@ export default {
 		const throttle = computed((): number => engineValue.value.throttle);
 		const coolant = computed((): number => engineValue.value.coolant);
 
+		// настройки отображения
+		const viewSettingModel = ref(false);
+		const viewSettingTitle = ref('');
+		const viewConfig = ref({ enabled: false, type: TViewType.VIEW_TEXT_SIMPLE, time: 0 } as IViewConfig);
+		let selectItemMenu: TItemMenu;
+
+		/** Выбор пункта меню отображения на информационном экране */
 		const onClickOptions = (e: any): void => {
-			console.log('EngineCard -> onClickOptions', e);
+			// console.log('EngineCard -> onClickOptions', e);
+
+			selectItemMenu = e.type;
+			switch (selectItemMenu) {
+				case TItemMenu.VIEW_ENGINE_ENABLED:
+					viewConfig.value = engineView.enabled;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_RPM:
+					viewConfig.value = engineView.rpm;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_COUNT_RPM:
+					viewConfig.value = engineView.totalCountRPM;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_LOAD:
+					viewConfig.value = engineView.load;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_MOTORS:
+					viewConfig.value = engineView.totalSeconds;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_THROTTLE:
+					viewConfig.value = engineView.throttle;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_COOLANT:
+					viewConfig.value = engineView.coolant;
+					break;
+			}
+
+			viewSettingTitle.value = e.lang;
+			viewSettingModel.value = true;
+		};
+
+		/**
+		 * Применить параметры отображения на информационном экране
+		 * @param {IViewConfig} res Новые параметры отображения
+		 */
+		const onApplyViewConfig = (res: IViewConfig): void => {
+			switch (selectItemMenu) {
+				case TItemMenu.VIEW_ENGINE_ENABLED:
+					engineView.enabled = res;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_RPM:
+					engineView.rpm = res;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_COUNT_RPM:
+					engineView.totalCountRPM = res;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_LOAD:
+					engineView.load = res;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_MOTORS:
+					engineView.totalSeconds = res;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_THROTTLE:
+					engineView.throttle = res;
+					break;
+
+				case TItemMenu.VIEW_ENGINE_COOLANT:
+					engineView.coolant = res;
+					break;
+			}
+			api.send(engineView);
 		};
 
 		return {
@@ -109,7 +207,11 @@ export default {
 			throttle,
 			coolant,
 			menuEngineCard,
-			onClickOptions
+			viewSettingModel,
+			viewSettingTitle,
+			viewConfig,
+			onClickOptions,
+			onApplyViewConfig
 		};
 	}
 };
