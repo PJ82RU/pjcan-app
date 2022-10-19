fb66df3e4a443ac03dfa1d31037350402fe7dd02
PJ82 <project82@mail.ru>
2022-10-19 22:02:03 +0500

    Add Card

---
+++ src/components/cards/Card.vue
@@ -0,0 +1,44 @@
+<template>
+	<v-card class="card" height="100%">
+		<v-card-text>
+			<slot name="body" />
+		</v-card-text>
+		<v-card-actions class="justify-space-between">
+			<div class="pl-4 pr-4 text-h5 text-uppercase">
+				{{ title }}
+			</div>
+			<v-btn-group class="border-dialog-btns">
+				<v-btn icon="mdi-close" color="#25323e" @click="$emit('click:close')" />
+			</v-btn-group>
+		</v-card-actions>
+	</v-card>
+</template>
+
+<script lang="ts">
+export default {
+	name: "Card",
+	props: {
+		title: String
+	},
+	emits: ["click:close"]
+};
+</script>
+
+<style lang="scss" scoped>
+.card
+{
+	width: 100%;
+	height: 100%;
+	background: #FFFFFF;
+	box-shadow: 0 4px 16px rgba(27, 44, 61, 0.18);
+	border-radius: 6px;
+
+	::v-deep(.v-card-text) {
+		height: calc(100% - 48px);
+		padding: 16px !important;
+	}
+	::v-deep(.v-btn .v-icon) {
+		color: white;
+	}
+}
+</style>
