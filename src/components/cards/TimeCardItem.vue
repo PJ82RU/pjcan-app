2cffdc6a4b90f5ad1087e793d221aca5201da7ef
PJ82 <project82@mail.ru>
2022-10-19 22:36:03 +0500

    Add TimeCardItem

---
+++ src/components/cards/TimeCardItem.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="time-card-item d-flex">
+		<v-text-field
+			class="fixed-message"
+			:model-value="title"
+			:hint="description"
+			variant="underlined"
+			density="compact"
+			persistent-hint
+			readonly
+			dense
+		/>
+		<v-text-field
+			class="time-card-item__time"
+			:model-value="value"
+			variant="underlined"
+			density="compact"
+			reverse
+			persistent-hint
+			readonly
+			dense
+		/>
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+	name: "TimeCardItem",
+	props: {
+		value: String,
+		title: String,
+		description: String
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.time-card-item {
+	&__time {
+		width: 70px;
+		font-family: "Jura", "Roboto", sans-serif;
+
+		::v-deep(input) {
+			color: $primary-color;
+		}
+	}
+}
+</style>
