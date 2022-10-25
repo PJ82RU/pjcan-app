2e474efa77efac517a300e6fe9127d34634edec5
PJ82 <project82@mail.ru>
2022-10-25 22:14:39 +0500

    Update InfoCard

--- src/views/onboard/components/InfoCard.vue
+++ src/views/onboard/components/InfoCard.vue
@@ -4,10 +4,10 @@
 			<v-row>
 				<v-col cols="12" class="pb-0">
 					<icon-card-item
-						:model-value="acc"
+						:model-value="[acc]"
 						:title="$t('onboard.info.acc.title')"
 						:description="$t('onboard.info.acc.description')"
-						icon-name="key"
+						:icon-name="['key']"
 					/>
 				</v-col>
 				<v-col cols="12" class="pt-0 pb-0">
@@ -22,6 +22,32 @@
 						:value="temperature"
 						:title="$t('onboard.info.temperature.title')"
 						:description="$t('onboard.info.temperature.description')"
+					/>
+				</v-col>
+				<v-col cols="12" class="pt-0 pb-0">
+					<switch-card-item
+						:model-value="handbrake"
+						:title="$t('onboard.info.handbrake.title')"
+						:description="$t('onboard.info.handbrake.description')"
+						color="error"
+					/>
+				</v-col>
+				<v-col cols="12" class="pt-0 pb-0">
+					<switch-card-item
+						:model-value="reverse"
+						:title="$t('onboard.info.reverse.title')"
+						:description="$t('onboard.info.reverse.description')"
+						color="warning"
+					/>
+				</v-col>
+				<v-col cols="12" class="pt-0 pb-0">
+					<icon-card-item
+						:model-value="[seatbeltPassenger, seatbeltDriver]"
+						:title="$t('onboard.info.safetyBelt.title')"
+						:description="$t('onboard.info.safetyBelt.description')"
+						:icon-name="['passenger', 'passenger']"
+						:colorsTrue="{ primary: '#66bb6a' }"
+						:colorsFalse="{ primary: '#ef5350' }"
 					/>
 				</v-col>
 			</v-row>
@@ -50,14 +76,22 @@ export default {
 		// температура
 		const temperatureValue = ref(new TemperatureValue());
 
-		const acc = computed((): boolean => sensorValue.value.acc);
+		const acc = computed((): boolean => true ?? sensorValue.value.acc);
 		const timeWork = computed((): string => "01:05:30");
 		const temperature = computed((): string => `${temperatureValue.value.out.toFixed(1)}°C`);
+		const handbrake = computed((): boolean => true ?? sensorValue.value.handbrake);
+		const reverse = computed((): boolean => true ?? sensorValue.value.reverse);
+		const seatbeltDriver = computed((): boolean => true ?? sensorValue.value.seatbeltDriver);
+		const seatbeltPassenger = computed((): boolean => sensorValue.value.seatbeltPassenger);
 
 		return {
 			acc,
 			timeWork,
-			temperature
+			temperature,
+			handbrake,
+			reverse,
+			seatbeltDriver,
+			seatbeltPassenger
 		};
 	}
 };
