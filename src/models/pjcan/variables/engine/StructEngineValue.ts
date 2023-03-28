75dd572c9e36ad784dd65b280b7b8b91be3d24ee
PJ82 <project82@mail.ru>
2023-03-28 20:28:34 +0500

    Update firmware to 4.0.0.89

--- src/models/pjcan/variables/engine/StructEngineValue.ts
+++ src/models/pjcan/variables/engine/StructEngineValue.ts
@@ -1,6 +1,6 @@
 import { BluetoothStruct } from "@/components/bluetooth";
 
-export const API_VARIABLE_ENGINE_SIZE = 22;
+export const API_VARIABLE_ENGINE_SIZE = 24;
 
 /** Структура данных */
 export const StructEngineValue = {
@@ -10,7 +10,7 @@ export const StructEngineValue = {
 	mseconds: BluetoothStruct.uint32(),
 	totalSeconds: BluetoothStruct.uint32(),
 	totalCountRPM: BluetoothStruct.uint32(),
-	countRPM: BluetoothStruct.uint16(),
+	countRPM: BluetoothStruct.uint32(),
 	load: BluetoothStruct.uint16(),
 	throttle: BluetoothStruct.uint16()
 };
