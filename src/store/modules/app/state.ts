996b9effaea06c5a7aebffc533b7ce68d5f78d3c
PJ82 <project82@mail.ru>
2024-01-11 16:35:22 +0500

    Move carModel in store

--- src/store/modules/app/state.ts
+++ src/store/modules/app/state.ts
@@ -1,9 +1,11 @@
 import onboardCardListDefault from "./onboard-card-list-default";
+import { TCarModel } from "@/models/pjcan/mazda";
 
 const state = {
 	messages: [],
 	visibleMessage: false,
-	onboardCardList: onboardCardListDefault
+	onboardCardList: onboardCardListDefault,
+	carModel: TCarModel.CAR_MODEL_UNKNOWN
 };
 
 export default state;
