6eaecbefcbb49276948fa09a180ecd5714a93507
PJ82 <project82@mail.ru>
2024-01-15 16:33:43 +0500

    Updated movement view

--- src/views/onboard/components/MovementCard.vue
+++ src/views/onboard/components/MovementCard.vue
@@ -2,7 +2,7 @@
 	<card
 		class="movement-card"
 		:title="$t('onboard.movement.title')"
-		:menu="carModel !== ECarModel.CAR_MODEL_MAZDA_CX9_REST ? menu : undefined"
+		:menu="carModel !== TCarModel.CAR_MODEL_MAZDA_CX9_REST ? menu : undefined"
 		@click:menu="onMenuClick"
 	>
 		<template #body>
@@ -12,8 +12,8 @@
 						:value="speed"
 						:title="$t('onboard.movement.speed.title')"
 						:description="$t('onboard.movement.speed.description')"
-						:nodata="!isLoadedValue"
-						:disabled="!isLoadedView"
+						:nodata="!isSpeed"
+						:disabled="!movementViewLoaded"
 					/>
 				</v-col>
 				<v-col cols="12" class="pt-0 pb-0">
@@ -22,7 +22,7 @@
 						:title="$t('onboard.movement.speedAVG.title')"
 						:description="$t('onboard.movement.speedAVG.description')"
 						:nodata="!isSpeedAVG"
-						:disabled="!isLoadedView"
+						:disabled="!movementViewLoaded"
 					/>
 				</v-col>
 				<v-col cols="12" class="pt-0 pb-0">
@@ -31,7 +31,7 @@
 						:title="$t('onboard.movement.restWay.title')"
 						:description="$t('onboard.movement.restWay.description')"
 						:nodata="!isRestWay"
-						:disabled="!isLoadedView"
+						:disabled="!movementViewLoaded"
 					/>
 				</v-col>
 			</v-row>
@@ -39,14 +39,12 @@
 	</card>
 
 	<view-setting-dialog
-        v-if="carModel !== ECarModel.CAR_MODEL_MAZDA_CX9_REST"
+		v-if="carModel !== TCarModel.CAR_MODEL_MAZDA_CX9_REST"
 		v-model="menuVisible"
 		:title="menuSelected.title"
-		:enabled="menuViewConfig.enabled"
-		:type="menuViewConfig.type"
-		:time="menuViewConfig.time"
-		:disabled="!isLoadedView"
-		@click:apply="onViewSettingApply"
+		:view="menuSelected.view"
+		:disabled="menuSelected.disabled"
+		@click:apply="onMovementViewApply"
 	/>
 </template>
 
@@ -58,91 +56,66 @@ import Card from "@/components/cards/Card.vue";
 import InputCardItem from "@/components/cards/InputCardItem.vue";
 import ViewSettingDialog from "./ViewSettingDialog.vue";
 import { IMenuItem } from "@/components/MenuDots.vue";
-import { ECarModel } from "@/models/pjcan/car";
+import { TCarModel } from "@/models/pjcan/mazda";
 
 import { IViewConfig } from "@/models/pjcan/view";
 import {
-	API_VARIABLE_MOVEMENT_EVENT,
-	API_VARIABLE_MOVEMENT_VIEW_EVENT,
-	API_VARIABLE_MOVEMENT_VIEW_EXEC,
+	API_MOVEMENT_VALUE_EVENT,
+	API_MOVEMENT_VIEW_EVENT,
 	IMovementValue,
-	IMovementView
-} from "@/models/pjcan/variables/movement";
+	IMovementViews,
+	MovementViews
+} from "@/models/pjcan/movement";
 
 import canbus from "@/api/canbus";
+import { API_CANBUS_EVENT } from "@/models/pjcan/base/BaseModel";
+import store from "@/store";
 
 export default {
 	name: "MovementCard",
 	computed: {
-		ECarModel()
+		TCarModel()
 		{
-			return ECarModel;
+			return TCarModel;
 		}
 	},
 	components: { Card, InputCardItem, ViewSettingDialog },
-	props: {
-		carModel: {
-			type: Number,
-			default: 0
-		}
-	},
 	setup()
 	{
 		const { t } = useI18n();
 
-		const isLoadedView = ref(false);
-		const isLoadedValue = ref(false);
+		const movementValueLoaded = ref(false);
+		const movementViewLoaded = ref(false);
 
 		const speed = ref("");
 		const speedAVG = ref("");
 		const restWay = ref("");
+		const isSpeed = ref(false);
+		const isSpeedAVG = ref(false);
+		const isRestWay = ref(false);
+		const carModel = computed((): TCarModel => store.getters["app/carModel"]);
 
-		const isSpeedAVG = computed(() => isLoadedValue.value && canbus.values.variable.movement.speedAVG > 0);
-		const isRestWay = computed(() => isLoadedValue.value && canbus.values.variable.movement.restWay > 0);
-
-		/** Входящие значения движения */
-		const onReceiveValue = (res: IMovementValue): void =>
-		{
-			isLoadedValue.value = res.isData;
-			if (res.isData)
-			{
-				speed.value = (res.speed / 100).toFixed(2);
-				speedAVG.value = res.speedAVG.toFixed(0);
-				restWay.value = (res.restWay / 100).toFixed(2);
-			}
-		};
-
-		/** Входящие значения отображения движения */
-		const onReceiveView = (res: IMovementView): void =>
-		{
-			isLoadedView.value = res.isData;
-		};
-
-		// регистрируем события
-		onMounted(() =>
-		{
-			canbus.addListener(API_VARIABLE_MOVEMENT_EVENT, onReceiveValue);
-			canbus.addListener(API_VARIABLE_MOVEMENT_VIEW_EVENT, onReceiveView);
-			onReceiveValue(canbus.values.variable.movement);
-			onReceiveView(canbus.views.variable.movement);
-		});
-		// удаляем события
-		onUnmounted(() =>
-		{
-			canbus.removeListener(API_VARIABLE_MOVEMENT_EVENT, onReceiveValue);
-			canbus.removeListener(API_VARIABLE_MOVEMENT_VIEW_EVENT, onReceiveView);
-		});
-
-		// МЕНЮ ОТОБРАЖЕНИЯ
+		let movementViews: IMovementViews;
 
 		const menu = computed((): IMenuItem[] => [
-			{ id: 0, title: t("onboard.movement.speed.menu") },
-			{ id: 1, title: t("onboard.movement.speedAVG.menu") },
-			{ id: 2, title: t("onboard.movement.restWay.menu") }
+			{
+				title: t("onboard.movement.speed.menu"),
+				view: movementViews?.speed,
+				disabled: !movementViewLoaded.value
+			},
+			{
+				title: t("onboard.movement.speedAVG.menu"),
+				view: movementViews?.speedAVG,
+				disabled: !movementViewLoaded.value
+			},
+			{
+				title: t("onboard.movement.restWay.menu"),
+				view: movementViews?.restWay,
+				disabled: !movementViewLoaded.value
+			}
 		]);
 		const menuVisible = ref(false);
 		const menuSelected = ref({} as IMenuItem);
-		const menuViewConfig = ref({} as IViewConfig);
 
 		/**
 		 * Выбор пункта меню отображения на информационном экране
@@ -152,62 +125,71 @@ export default {
 		{
 			menuVisible.value = true;
 			menuSelected.value = item;
-
-			const { movement } = canbus.views.variable;
-			switch (item.id)
-			{
-				case 0:
-					menuViewConfig.value = movement.speed;
-					return;
-
-				case 1:
-					menuViewConfig.value = movement.speedAVG;
-					break;
-
-				case 2:
-					menuViewConfig.value = movement.restWay;
-					break;
-			}
 		};
 
 		/**
 		 * Применить параметры отображения на информационном экране
 		 * @param {IViewConfig} data Новые параметры отображения
 		 */
-		const onViewSettingApply = (data: IViewConfig): void =>
+		const onMovementViewApply = (data: IViewConfig): void =>
 		{
-			const { movement } = canbus.views.variable;
-			switch (menuSelected.value.id)
-			{
-				case 0:
-					movement.speed = data;
-					break;
-
-				case 1:
-					movement.speedAVG = data;
-					break;
-
-				case 2:
-					movement.restWay = data;
-					break;
-			}
-			canbus.queryView(API_VARIABLE_MOVEMENT_VIEW_EXEC);
+			canbus.query(data);
 		};
 
+		/** Входящие значения движения */
+		const onMovementValueReceive = (res: IMovementValue): void =>
+		{
+			movementValueLoaded.value = res.isData;
+			if (res.isData)
+			{
+				speed.value = (res.speed / 100).toFixed(2);
+				speedAVG.value = res.speedAVG.toFixed(0);
+				restWay.value = (res.restWay / 100).toFixed(2);
+				isSpeed.value = res.speed > 0;
+				isSpeedAVG.value = res.speedAVG > 0;
+				isRestWay.value = res.restWay > 0;
+			}
+		};
+		/** Входящие значения отображения движения */
+		const onMovementViewReceive = (res: IMovementViews): void =>
+		{
+			movementViewLoaded.value = res.isData;
+			movementViews = res;
+		};
+
+		const onBegin = (status: boolean): void =>
+		{
+			if (status) canbus.query(new MovementViews(), true);
+		};
+		onMounted(() =>
+		{
+			canbus.addListener(API_MOVEMENT_VALUE_EVENT, onMovementValueReceive);
+			canbus.addListener(API_MOVEMENT_VIEW_EVENT, onMovementViewReceive);
+			canbus.addListener(API_CANBUS_EVENT, onBegin);
+			onBegin(canbus.begin);
+		});
+		onUnmounted(() =>
+		{
+			canbus.removeListener(API_MOVEMENT_VALUE_EVENT, onMovementValueReceive);
+			canbus.removeListener(API_MOVEMENT_VIEW_EVENT, onMovementViewReceive);
+			canbus.removeListener(API_CANBUS_EVENT, onBegin);
+		});
+
 		return {
-			isLoadedView,
-			isLoadedValue,
-			isSpeedAVG,
-			isRestWay,
+			movementViewLoaded,
+			movementValueLoaded,
 			speed,
 			speedAVG,
 			restWay,
+			isSpeed,
+			isSpeedAVG,
+			isRestWay,
+			carModel,
 			menu,
 			menuVisible,
 			menuSelected,
-			menuViewConfig,
 			onMenuClick,
-			onViewSettingApply
+			onMovementViewApply
 		};
 	}
 };
