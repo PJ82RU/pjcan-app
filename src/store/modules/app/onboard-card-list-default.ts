2d751d28f015155633a7bf6ee0046b10a0ea1377
PJ82 <project82@mail.ru>
2024-01-15 17:34:32 +0500

    Updated card list default

--- src/store/modules/app/onboard-card-list-default.ts
+++ src/store/modules/app/onboard-card-list-default.ts
@@ -1,11 +1,58 @@
 import { IOnboardCard } from "@/models/interfaces/IOnboardCard";
+import { TCarModel } from "@/models/pjcan/mazda";
 
 export default [
-	{ name: "info", enabled: true, car: [1, 2, 3, 4] },
-	{ name: "bose", enabled: true, car: [2, 3] },
-	{ name: "engine", enabled: true, car: [1, 2] },
-	{ name: "fuel", enabled: true, car: [1, 2, 3, 4] },
-	{ name: "movement", enabled: true, car: [1, 2, 3, 4] },
-	{ name: "doors", enabled: true, car: [1] },
-	{ name: "climate", enabled: true, car: [1, 2] }
+	{
+		name: "info",
+		enabled: true,
+		car: [
+			TCarModel.CAR_MODEL_MAZDA_3_BK,
+			TCarModel.CAR_MODEL_MAZDA_3_BL,
+			TCarModel.CAR_MODEL_MAZDA_6_GG,
+			TCarModel.CAR_MODEL_MAZDA_6_GH,
+			TCarModel.CAR_MODEL_MAZDA_CX7,
+			TCarModel.CAR_MODEL_MAZDA_CX7_REST,
+			TCarModel.CAR_MODEL_MAZDA_CX9,
+			TCarModel.CAR_MODEL_MAZDA_CX9_REST
+		]
+	},
+	{
+		name: "bose",
+		enabled: true,
+		car: [TCarModel.CAR_MODEL_MAZDA_CX7, TCarModel.CAR_MODEL_MAZDA_CX7_REST, TCarModel.CAR_MODEL_MAZDA_CX9]
+	},
+	{
+		name: "engine",
+		enabled: true,
+		car: [
+			TCarModel.CAR_MODEL_MAZDA_3_BK,
+			TCarModel.CAR_MODEL_MAZDA_CX7,
+			TCarModel.CAR_MODEL_MAZDA_CX7_REST,
+			TCarModel.CAR_MODEL_MAZDA_CX9,
+			TCarModel.CAR_MODEL_MAZDA_CX9_REST
+		]
+	},
+	{ name: "fuel", enabled: true, car: [
+		TCarModel.CAR_MODEL_MAZDA_3_BK,
+		TCarModel.CAR_MODEL_MAZDA_CX7,
+		TCarModel.CAR_MODEL_MAZDA_CX7_REST,
+		TCarModel.CAR_MODEL_MAZDA_CX9,
+		TCarModel.CAR_MODEL_MAZDA_CX9_REST
+	] },
+	{ name: "movement", enabled: true, car: [
+		TCarModel.CAR_MODEL_MAZDA_3_BK,
+		TCarModel.CAR_MODEL_MAZDA_CX7,
+		TCarModel.CAR_MODEL_MAZDA_CX7_REST,
+		TCarModel.CAR_MODEL_MAZDA_CX9,
+		TCarModel.CAR_MODEL_MAZDA_CX9_REST
+	] },
+	{ name: "doors", enabled: true, car: [TCarModel.CAR_MODEL_MAZDA_3_BK] },
+	{ name: "climate", enabled: true, car: [
+		TCarModel.CAR_MODEL_MAZDA_3_BK,
+		TCarModel.CAR_MODEL_MAZDA_6_GG,
+		TCarModel.CAR_MODEL_MAZDA_CX7,
+		TCarModel.CAR_MODEL_MAZDA_CX7_REST,
+		TCarModel.CAR_MODEL_MAZDA_CX9,
+		TCarModel.CAR_MODEL_MAZDA_CX9_REST
+	] }
 ] as IOnboardCard[];
